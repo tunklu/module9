5ac0c91928a400c945e1c86a97b93a52e0b3c746
Denis <vin_ogr@list.ru>
2024-11-14 14:33:05 +0700

    Генераторные сборки

--- module_9_3/module_9_3.py
+++ module_9_3/module_9_3.py
@@ -1,6 +1,21 @@
+
+
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-first_result = ''
+first_result = (len(i)-len(j) for i,j in zip(first, second) if len(i)!=len(j))
 
-second_result = ''
+second_result=[]
+for i in range(0, len(first)):
+        if (len(first[i])==len(second[i])):
+            second_result.append(True)
+        else:
+            second_result.append(False)
+
+second_result = (True if len(first[i])==len(second[i]) else False for i in range(0, len(first)))
+
+print(first_result)
+print(list(first_result))
+
+print(second_result)
+print(list(second_result))
