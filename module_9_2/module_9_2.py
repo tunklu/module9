69c2b83552f99eb4dd2f0086e83660c97572f9fc
Denis <vin_ogr@list.ru>
2024-11-14 14:54:55 +0700

    Списковые, словарные сборки

--- module_9_2/module_9_2.py
+++ module_9_2/module_9_2.py
@@ -1,22 +1,19 @@
 first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
 
-first_result = []
-for i in first_strings:
-    if len(i) > 5:
-        first_result.append(len(i))
 
-second_result = []
-for i in first_strings:
-    for j in second_strings:
-        if len(i) == len(j):
-            second_result.append([i ,j])
+
+first_result = (len(i) for i in first_strings if len(i)>=5)
+
+second_result = ((i,j) for i in first_strings for j in second_strings if len(i) == len(j))
 
 third_result = {}
 for i in first_strings + second_strings:
     if (len(i)%2) == 0:
         third_result[i] = len(i)
 
-print(first_result)
-print(second_result)
+third_result = dict((i,len(i)) for i in (first_strings+second_strings) if (len(i)%2) == 0)
+
+print(list(first_result))
+print(list(second_result))
 print(third_result)
